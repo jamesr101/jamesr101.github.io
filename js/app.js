213f56ce3a0b3ca93c29710e1abf703b582f3cb0
James Robins <jamesrobins101@hotmail.com>
2018-12-31 18:44:15 +0000

    created animated nav bar

--- js/app.js
+++ js/app.js
@@ -1,1 +1,25 @@
-console.log('Hello world!')
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("DOM fully loaded and parsed");
+    const navbar = document.querySelector('nav')
+    console.dir(navbar)
+
+    window.addEventListener('scroll', checkScroll)
+
+
+    var lastKnownScrollY = 0
+
+    function checkScroll() {
+      if (lastKnownScrollY - window.scrollY > 0) {
+      console.count('going up!')
+      navbar.classList.remove('minimised')
+
+      }
+
+      if (lastKnownScrollY - window.scrollY < 0) {
+        console.count('going down!')
+        navbar.classList.add('minimised')
+      }
+      lastKnownScrollY = window.scrollY
+    }
+  });
