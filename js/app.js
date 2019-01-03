9cbef06cb069753ddff8c38e2c6f4a9dd5f7918b
James Robins <jamesrobins101@hotmail.com>
2019-01-03 19:39:49 +0000

    added basic transitions

--- js/app.js
+++ js/app.js
@@ -22,4 +22,26 @@ document.addEventListener("DOMContentLoaded", function() {
       }
       lastKnownScrollY = window.scrollY
     }
+
+
+
+
+    window.addEventListener('scroll', checkSlide)
+
+    const sliderImages = document.querySelectorAll('.transition');
+
+    function checkSlide() {
+      sliderImages.forEach(sliderImage => {
+        // half way through the image
+        const slideInAt = (window.scrollY + window.innerHeight) - (sliderImage.height || sliderImage.scrollHeight) / 2;
+        // bottom of the image
+        // const imageBottom = sliderImage.offsetTop + (sliderImage.height || sliderImage.scrollHeight);
+        const isHalfShown = slideInAt > sliderImage.offsetTop;
+        // const isNotScrolledPast = window.scrollY < imageBottom;
+        if (isHalfShown) {
+          sliderImage.classList.add('active');
+        }
+      });
+    }
+
   });
