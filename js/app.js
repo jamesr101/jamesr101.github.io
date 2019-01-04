8e188e1a77fc5fc9826db4bce2076ae174a48d7a
James Robins <jamesrobins101@hotmail.com>
2019-01-04 20:01:41 +0000

    added burger menu

--- js/app.js
+++ js/app.js
@@ -49,4 +49,28 @@ document.addEventListener("DOMContentLoaded", function() {
       checkNav()
     }))
 
+    var nav = document.querySelector("nav ul"),
+    navToggle = document.querySelector("nav .skip");
+
+    if (navToggle) {
+      navToggle.addEventListener("click",
+      function(e) {
+        if (nav.className == "open") {
+          nav.className = "";
+        } else {
+          nav.className = "open";
+        }
+        e.preventDefault();
+      }, false);
+    }
+    
+    /* Any click outside of the nav element will close the menu if it's open */
+    var specifiedElement = document.querySelector('nav');
+    document.addEventListener('click', function(event) {
+      var isClickInside = specifiedElement.contains(event.target);
+      if (!isClickInside && nav.className == "open") {
+        nav.className = "";
+      }
+});
+
   });
